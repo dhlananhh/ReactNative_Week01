e313bfdc09908f66c100d9d6b070cd26438973ba
dhlananhh <dhlananh2309@gmail.com>
2024-08-26 13:07:01 +0700

    update week1 javascript exercises

--- 3. Data Structures, Modern Operators and Strings/exercise03.js
+++ 3. Data Structures, Modern Operators and Strings/exercise03.js
@@ -9,14 +9,14 @@
 
     Your tasks:
         1. Create an array 'events' of the different game events that happened 
-        (no duplicates)
+    (no duplicates)
         2. After the game has finished, is was found that the yellow card from 
-        minute 64 was unfair. So remove this event from the game events log.
+    minute 64 was unfair. So remove this event from the game events log.
         3. Compute and log the following string to the console: "An event happened, on
-        average, every 9 minutes" (keep in mind that a game has 90 minutes)
+    average, every 9 minutes" (keep in mind that a game has 90 minutes)
         4. Loop over 'gameEvents' and log each element to the console, marking
-        whether it's in the first half or second half (after 45 min) of the game, like this:
-            [FIRST HALF] 17: ⚽ GOAL
+    whether it's in the first half or second half (after 45 min) of the game, like this:
+        [FIRST HALF] 17: ⚽ GOAL
 */
 
 
