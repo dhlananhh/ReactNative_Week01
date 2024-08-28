16c5565e9ae7353055623781a9fcd4a088ea20f2
dhlananhh <dhlananh2309@gmail.com>
2024-08-28 20:20:09 +0700

    update week1 oop javascript exercises

--- 6. Object Oriented Programming (OOP)/exercise01.js
+++ 6. Object Oriented Programming (OOP)/exercise01.js
@@ -16,3 +16,33 @@
 */
 
 
+// 1. Constructor function for Car
+function Car(make, speed) {
+    this.make = make; // The make of the car
+    this.speed = speed; // The current speed of the car in km/h
+}
+
+// 2. Method to accelerate the car
+Car.prototype.accelerate = function() {
+    this.speed += 10; // Increase speed by 10
+    console.log(`${this.make} is going at ${this.speed} km/h`); // Log new speed
+};
+
+// 3. Method to brake the car
+Car.prototype.brake = function() {
+    this.speed -= 5; // Decrease speed by 5
+    console.log(`${this.make} is going at ${this.speed} km/h`); // Log new speed
+};
+
+// 4. Create two Car objects
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+// Experiment with calling accelerate and brake
+car1.accelerate(); // BMW is going at 130 km/h
+car1.accelerate(); // BMW is going at 140 km/h
+car1.brake();      // BMW is going at 135 km/h
+
+car2.accelerate(); // Mercedes is going at 105 km/h
+car2.brake();      // Mercedes is going at 100 km/h
+car2.brake();      // Mercedes is going at 95 km/h
